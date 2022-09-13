34b76f7259630dcf845d3e29287e4065f47877ad
GrauEd <graue@educ.college-montral.qc.ca>
2022-09-13 08:59:31 -0400

    TP1

--- TP1.py
+++ TP1.py
@@ -4,4 +4,10 @@
 phrase = input("Ecrivez une phrase: ")
 
 #afficher la phrase du input
-print("votre phrse est : "
+print("votre phrase est : " + phrase)
+
+#compteur de mot
+word_count = len(phrase.split())
+
+#afficher le nombre de mots en string
+print("le nombre de mots est : " + str(word_count))

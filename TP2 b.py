8512033fa559cc2b3b8102e6d54c9486d1a08a45
graue <graue@educ.college-montreal.qc.ca>
2022-10-12 09:03:35 -0400

    TP2

--- TP2 b.py
+++ TP2 b.py
@@ -1,19 +1,29 @@
 #Edward Grau
 #TP2 #2
 #gr.401
+from random import randint
 
 print("J’ai choisi un nombre au hasard entre 1 et 100. À vous de le deviner...")
-
+nb_essais = 0
 nombre = randint(0,100)
-Essai = input("Entrez votre essai : ")
+Essai = int(input("Entrez votre essai : "))
+nb_essais = nb_essais+1
 
-if devine < nombre:
-    print("Mauvais choix, le nombre est plus petit que" + Essai)
+
+if Essai < nombre:
+    print("Mauvais choix, le nombre est plus petit que" + str(Essai))
     input("Entrez votre essai : ")
 
-elif devine > nombre:
-    print("Mauvaise réponse, le nombre est plus grand que" + Essai)
+elif Essai > nombre:
+    print("Mauvaise réponse, le nombre est plus grand que" + str(Essai))
     input("Entrez votre essai : ")
 
-elif devine == nombre:
-    print("")
+elif Essai == nombre:
+    print("Bravo! Bonne réponse")
+    print('Vous avez réussi en :' + nb_essais)
+
+replay = input('Veux-tu rejouer? (y/n)')
+
+if
+
+elif

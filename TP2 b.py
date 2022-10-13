97c3da12e53379d50bae0a670430bebe4ddab619
graue <graue@educ.college-montreal.qc.ca>
2022-10-13 09:50:24 -0400

    TP2

--- TP2 b.py
+++ TP2 b.py
@@ -3,27 +3,34 @@
 #gr.401
 from random import randint
 
-print("J’ai choisi un nombre au hasard entre 1 et 100. À vous de le deviner...")
-nb_essais = 0
-nombre = randint(0,100)
-Essai = int(input("Entrez votre essai : "))
-nb_essais = nb_essais+1
+while True:
+    print("J’ai choisi un nombre au hasard entre 1 et 100. À vous de le deviner...")
+    nb_essais = 0
+    nombre = randint(0,100)
+    Essai = int(input("Entrez votre essai : "))
+    nb_essais = nb_essais+1
 
 
-if Essai < nombre:
-    print("Mauvais choix, le nombre est plus petit que" + str(Essai))
-    input("Entrez votre essai : ")
+    if Essai < nombre:
+        print("Mauvais choix, le nombre est plus petit que" + str(Essai))
+        input("Entrez votre essai : ")
 
-elif Essai > nombre:
-    print("Mauvaise réponse, le nombre est plus grand que" + str(Essai))
-    input("Entrez votre essai : ")
+    elif Essai > nombre:
+        print("Mauvaise réponse, le nombre est plus grand que" + str(Essai))
+        input("Entrez votre essai : ")
 
-elif Essai == nombre:
-    print("Bravo! Bonne réponse")
-    print('Vous avez réussi en :' + nb_essais)
+    elif Essai == nombre:
+        print("Bravo! Bonne réponse")
+        print('Vous avez réussi en :' + nb_essais)
 
-replay = input('Veux-tu rejouer? (y/n)')
+    replay = input('Veux-tu rejouer? (y/n)')
 
-if
+    if replay == 'y':
+        continue
 
-elif
+    elif replay == 'n':
+        print("Au revoir...")
+        break
+    else:
+        print("input invalide")
+        break

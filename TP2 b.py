4eadc8789c184fb290b12f55bf265119632438ce
graue <graue@educ.college-montreal.qc.ca>
2022-10-21 16:20:58 -0400

    tp2

--- TP2 b.py
+++ TP2 b.py
@@ -2,36 +2,43 @@
 #TP2 #2
 #gr.401
 from random import randint
-
-while True:
-    print("J’ai choisi un nombre au hasard entre 1 et 100. À vous de le deviner...")
-    nb_essais = 0
+nb_essais = 0
+def jeux():
     nombre = randint(0,100)
     Essai = int(input("Entrez votre essai : "))
-    nb_essais = nb_essais+1
+    print(str(nombre) + str(Essai))
 
-    print(str(nombre)+str(Essai))
-    if Essai > nombre:
-        print("Mauvais choix, le nombre est plus petit que : " + str(Essai))
-        print("Nombre d'éssais : " + str(nb_essais))
+    def game():
+       print("J’ai choisi un nombre au hasard entre 1 et 100. À vous de le deviner...")
+
+    global nb_essais
+    if Essai != nombre:
+
+        nb_essais += 1
 
 
-    elif Essai < nombre:
-        print("Mauvaise réponse, le nombre est plus grand que : " + str(Essai))
-        print("Nombre d'éssais : " + str(nb_essais))
+        if Essai > nombre:
+            print("Mauvais choix, le nombre est plus petit que : " + str(Essai))
+            print("Nombre d'éssais : " + str(nb_essais))
 
-    elif Essai == nombre:
-        print("Bravo! Bonne réponse")
-        print('Vous avez réussi en : ' + str(nb_essais))
 
-    replay = input('Veux-tu rejouer? (y/n)')
+        elif Essai < nombre:
+            print("Mauvaise réponse, le nombre est plus grand que : " + str(Essai))
+            print("Nombre d'éssais : " + str(nb_essais))
 
-    if replay == 'y':
-        continue
+        elif Essai == nombre:
+            print("Bravo! Bonne réponse")
+            print('Vous avez réussi en : ' + str(nb_essais))
 
-    elif replay == 'n':
-        print("Au revoir...")
-        break
-    else:
-        print("input invalide")
-        break
+        replay = input('Veux-tu rejouer? (y/n)')
+
+        if replay == 'y':
+            game()
+
+        elif replay == 'n':
+            print("Au revoir...")
+
+        else:
+            print("input invalide")
+
+jeux()

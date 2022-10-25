84ac1c666fe7120575da3af81d7fd2a5542ca309
graue <graue@educ.college-montrel.qc.ca>
2022-10-25 09:22:09 -0400

    tp2

--- TP2 b.py
+++ TP2 b.py
@@ -2,43 +2,50 @@
 #TP2 #2
 #gr.401
 from random import randint
-nb_essais = 0
+
+
 def jeux():
+    global nb_essais +
     nombre = randint(0,100)
-    Essai = int(input("Entrez votre essai : "))
-    print(str(nombre) + str(Essai))
+    nb_essais = 0
+    print(str(nombre))
+    print("J’ai choisi un nombre au hasard entre 1 et 100. À vous de le deviner...")
+    game(nombre)
 
-    def game():
-       print("J’ai choisi un nombre au hasard entre 1 et 100. À vous de le deviner...")
 
+def game(nombre):
     global nb_essais
-    if Essai != nombre:
+    Essai = int(input("Entrez votre essai : "))
+    nb_essais += 1
 
-        nb_essais += 1
+    if Essai > nombre:
+        print("Mauvais choix, le nombre est plus petit que : " + str(Essai))
+        print("Nombre d'éssais : " + str(nb_essais))
+        print('réessaye')
+        game(nombre)
 
 
-        if Essai > nombre:
-            print("Mauvais choix, le nombre est plus petit que : " + str(Essai))
-            print("Nombre d'éssais : " + str(nb_essais))
+
+    elif Essai < nombre:
+        print("Mauvaise réponse, le nombre est plus grand que : " + str(Essai))
+        print("Nombre d'éssais : " + str(nb_essais))
+        print('réessaye')
+        game(nombre)
 
 
-        elif Essai < nombre:
-            print("Mauvaise réponse, le nombre est plus grand que : " + str(Essai))
-            print("Nombre d'éssais : " + str(nb_essais))
-
-        elif Essai == nombre:
-            print("Bravo! Bonne réponse")
-            print('Vous avez réussi en : ' + str(nb_essais))
-
+    elif Essai == nombre:
+        print("Bravo! Bonne réponse")
+        print('Vous avez réussi en : ' + str(nb_essais))
         replay = input('Veux-tu rejouer? (y/n)')
 
         if replay == 'y':
-            game()
+           jeux()
 
         elif replay == 'n':
             print("Au revoir...")
 
         else:
             print("input invalide")
+
 
 jeux()

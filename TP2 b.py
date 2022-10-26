f244fe416ca7adc24346bdda26fd174280a1c833
graue <graue@educ.college-montrel.qc.ca>
2022-10-26 09:42:50 -0400

    tp2

--- TP2 b.py
+++ TP2 b.py
@@ -4,33 +4,32 @@
 from random import randint
 
 
-def jeux():
-    global nb_essais +
+def jeux(): #loop qui recommence le jeux
+    global nb_essais
     nombre = randint(0,100)
     nb_essais = 0
-    print(str(nombre))
     print("J’ai choisi un nombre au hasard entre 1 et 100. À vous de le deviner...")
     game(nombre)
 
 
-def game(nombre):
+def game(nombre): #loop qui commence une devinette
     global nb_essais
-    Essai = int(input("Entrez votre essai : "))
-    nb_essais += 1
+    Essai = int(input("Entrez votre essai : ")) #prendre l'input de l'utilisateur
+    nb_essais += 1 #augmenter le nombre d'essais
 
-    if Essai > nombre:
+    if Essai > nombre: #si la devinette est trop grand
         print("Mauvais choix, le nombre est plus petit que : " + str(Essai))
         print("Nombre d'éssais : " + str(nb_essais))
         print('réessaye')
-        game(nombre)
+        game(nombre) #redeviner
 
 
 
-    elif Essai < nombre:
+    elif Essai < nombre: #l'essais est trop petit
         print("Mauvaise réponse, le nombre est plus grand que : " + str(Essai))
         print("Nombre d'éssais : " + str(nb_essais))
         print('réessaye')
-        game(nombre)
+        game(nombre) #redeviner
 
 
     elif Essai == nombre:
@@ -39,7 +38,7 @@ def game(nombre):
         replay = input('Veux-tu rejouer? (y/n)')
 
         if replay == 'y':
-           jeux()
+           jeux() #refaire le jeux avec un nouveau chiffre
 
         elif replay == 'n':
             print("Au revoir...")

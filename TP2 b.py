513ec07ca039e5802a7b33a9b6059f87f7a8cd23
graue <graue@educ.college-montrel.qc.ca>
2022-11-08 09:19:26 -0500

    tp2 c)

--- TP2 b.py
+++ TP2 b.py
@@ -3,46 +3,65 @@
 #gr.401
 from random import randint
 
-
-def jeux(): #loop qui recommence le jeux
+#loop qui recommence le jeux
+def jeux():
+    #donner le choix de choisir les bornes de la devinette
+    bornes = int(input ("Voulez-vous choisir vos bornes de la devinette? 1(oui) 2(non)"))
+    if bornes == 1:
+        #borne 1
+        C1 = int(input("inscrivez votre première borne"))
+        #borne 2
+        C2 = int(input("inscrivez votre deuxième borne"))
+        nombre = randint(C1,C2)
+        print("J’ai choisi un nombre au hasard entre " + str(C1) + " et " + str(C2) + ". À vous de le deviner...")
+    elif bornes != 1:
+        nombre = randint(0, 100)
+        print("J’ai choisi un nombre au hasard entre 1 et 100. À vous de le deviner...")
+    #réinitialiser le nombre de "guess" après chaque partie
     global nb_essais
-    nombre = randint(0,100)
     nb_essais = 0
-    print("J’ai choisi un nombre au hasard entre 1 et 100. À vous de le deviner...")
+
     game(nombre)
 
-
-def game(nombre): #loop qui commence une devinette
+#loop qui commence une devinette
+def game(nombre):
     global nb_essais
-    Essai = int(input("Entrez votre essai : ")) #prendre l'input de l'utilisateur
-    nb_essais += 1 #augmenter le nombre d'essais
-
-    if Essai > nombre: #si la devinette est trop grand
+    # prendre l'input de l'utilisateur
+    Essai = int(input("Entrez votre essai : "))
+    # augmenter le nombre d'essais
+    nb_essais += 1
+    # si la devinette est trop grand
+    if Essai > nombre:
         print("Mauvais choix, le nombre est plus petit que : " + str(Essai))
         print("Nombre d'éssais : " + str(nb_essais))
         print('réessaye')
-        game(nombre) #redeviner
+        # redeviner
+        game(nombre)
 
 
-
-    elif Essai < nombre: #l'essais est trop petit
+    # l'essais est trop petit
+    elif Essai < nombre:
         print("Mauvaise réponse, le nombre est plus grand que : " + str(Essai))
         print("Nombre d'éssais : " + str(nb_essais))
         print('réessaye')
+        # redeviner
         game(nombre) #redeviner
 
-
+    #bonne réponse
     elif Essai == nombre:
         print("Bravo! Bonne réponse")
         print('Vous avez réussi en : ' + str(nb_essais))
         replay = input('Veux-tu rejouer? (y/n)')
 
+        # refaire le jeux avec un nouveau chiffre
         if replay == 'y':
-           jeux() #refaire le jeux avec un nouveau chiffre
+           jeux()
 
+        # terminer la partie
         elif replay == 'n':
             print("Au revoir...")
 
+        #réponse invalide
         else:
             print("input invalide")
 
---
+++ TP3.py
@@ -0,0 +1,14 @@
+niveau_vie = 20
+
+from random import randint
+de = randint(1,6)
+
+print("Vous tombez face à face avec un adversaire de difficulté : force_adversaire")
+
+menu = input("""Que voulez-vous faire ? 
+    1- Combattre cet adversaire
+	2- Contourner cet adversaire et aller ouvrir une autre porte
+	3- Afficher les règles du jeu
+	4- Quitter la partie""")
+
+if menu = "1":

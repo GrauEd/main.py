977ee7e6247c77e9e9e2dedafd9b80626f934ea2
graue <graue@educ.college-montreal.qc.ca>
2022-12-16 16:21:12 -0500

    tp4

---
+++ tp4.py
@@ -0,0 +1,49 @@
+#Groupe 401
+#Exercices a faire
+
+#Exercice a faire #1
+class StringFoo:
+    def __init__(self):
+        self.string = ""
+
+    def set_string(self, string):
+        self.string = string
+
+    def print_string(self):
+        print(self.string)
+
+#Exercice a faire #2
+class Rectangle:
+    def __init__(self, largeur, longueur):
+        self.largeur = largeur
+        self.longueur = longueur
+
+    def calcul_aire(self):
+        return self.largeur * self.longueur
+
+    def afficher_infos(self):
+#Exercice a faire #3
+import math
+aire = calcul.calcul_aire(5)
+circonference = calcul.calcul_circonference(5)
+
+print(aire)
+print(circonference)
+
+        print("Largeur :", self.largeur)
+        print("Longueur :", self.longueur)
+        print("Aire :", self.calcul_aire())
+rectangle = Rectangle(5,10)
+rectangle.affiche r_infos()
+
+
+
+class circle:
+    def calcul_aire(self, rayon):
+        return math.pi * (rayon ** 2)
+
+    def calcul_circonference_cercle(self, rayon):
+        return 2 * math.pi * rayon
+calcul = circle()
+
+#Exercise a faire 4

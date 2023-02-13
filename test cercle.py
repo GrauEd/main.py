bc5d86b613bbfcb65b285c372c78daf54075b480
graue <graue@educ.college-montreal.qc.ca>
2023-02-13 09:17:16 -0500

    test

---
+++ test cercle.py
@@ -0,0 +1,20 @@
+import arcade
+import random
+
+class MyGame(arcade.Window):
+   def __init__(self, width, height, title):
+       # Call the parent class's init function
+       super().__init__(width, height, title)
+
+
+def main():
+    window = MyGame(640, 480, "Drawing Example")
+    arcade.start_render()
+    for i in range(20):
+        arcade.draw_circle_filled(random.randint(50,540), random.randint(50, 380), random.randint(10,50), (random.randint(130,540),random.randint(1,380), random.randint(50,540)))
+    arcade.run()
+
+main()
+
+
+
